6f7e98a9937de963c200bc41e4de10efa147c554
asosh88 <36711932+asosh88@users.noreply.github.com>
2023-06-27 00:29:40 -0400

    Add hierarchical options.
    There are now 2 levels of options in the app.
    
    The first option, serivce_type, determines if Moodica acts like a search engine or a music recommender.
    The values for this first option can be "Search" or "Recommendation".
    If the first level option is "Search", the second level options are:
    
    1. 'Artists', 2. 'Albums', 3. 'Songs', and 4. 'Lyrics'
    
    If the first level option is "Recommendation", the seond level options are:
    
    1. 'Song' and 2. 'Playlist'.

--- App.py
+++ App.py
@@ -7,19 +7,37 @@ def app():
     st.title('Welcome to Moodica!')
     st.markdown('*Music Search & Recommendation*')
     
+    service_type = st.radio(
+    'Service Type:',
+    ('Search', 'Recommendation')
+    )
     
-    keyword = st.text_input('Keywords:')
+    if service_type == 'Search':
+        search_type = st.radio(
+        'Search for:',
+        ('Artists', 'Albums', 'Songs', 'Lyrics'),
+        horizontal=True
+        )
     
-    if keyword != '':
-        st.write('\n\n')
-        st.write(f'Showing Results for "{keyword}":')
+        keyword = st.text_input('Keywords:')
+
+        if keyword != '':
+            st.write('\n\n')
+            st.write(f'Showing Results for "{keyword}":')
+
+            for i, r in enumerate(Database.kw_search(keyword, search_type)):
+                st.markdown(f'**{i+1}. {r[0]}** | {r[1]}')
+                yr = str(r[3])[:-2]
+                st.markdown(f'Album: *{r[2]} ({yr})*')
+                st.markdown('\n\n')
     
-        for i, r in enumerate(Database.kw_search(keyword)):
-            st.markdown(f'**{i+1}. {r[0]}** | {r[1]}')
-            yr = str(r[3])[:-2]
-            st.markdown(f'Album: *{r[2]} ({yr})*')
-            st.markdown('\n\n')
-    
-    
+    if service_type == 'Recommendation':
+        recommendation_type = st.radio(
+        'Search for A:',
+        ('Song', 'Playlist'),
+        horizontal=True
+        )
+
+        
 if __name__ == '__main__':
     app()
