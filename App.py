a5a9c2d299e7557c3a07ac35a5520a8715aaa4e5
asosh88 <36711932+asosh88@users.noreply.github.com>
2023-06-29 07:00:35 -0400

    Add text field for "Recommendation" service_type. Add predictive typing for song search for recommendation.
    Define 2 new functions. The 1st one catches all song names. The second catches all unique artist
    names.

--- App.py
+++ App.py
@@ -3,16 +3,28 @@ import Database
 import Display
 import pandas as pd
 from urllib.parse import unquote, quote
+from streamlit_tags import st_tags, st_tags_sidebar
+import re
 
-def app():
+
+def on_click():
+    st.session_state['service_type'] = 'Recommendation'
+
+def app(state=0):
     
     st.title('Welcome to Moodica!')
     st.markdown('*Music Search & Recommendation*')
     
-    service_type = st.sidebar.radio(
+    #service_type_options = 
+    
+    service_type =  st.sidebar.radio(
     'Service Type:',
-    ('Search', 'Recommendation')
+    ('Search', 'Recommendation'),
+    index=state,
+    #key='service_type'
     )
+    
+    st.session_state['service_type'] = service_type
     
     sort_by = st.sidebar.radio(
     'Sort by:',
@@ -26,7 +38,8 @@ def app():
     index=1
     )
     
-    if service_type == 'Search':
+    
+    if st.session_state['service_type'] == 'Search':
         search_type = st.radio(
         'Search for:',
         ('Artists', 'Albums', 'Songs', 'Lyrics', 'Credits'),
@@ -34,7 +47,9 @@ def app():
         index=3
         )
     
-        keyword = st.text_input('Keywords:')        
+        placeholder = st.empty()
+        
+        keyword = placeholder.text_input('Keywords:', placeholder='Type your keywords here.')        
 
         field = {'Artists': 'Artist Name', 'Albums': 'Album Name', 'Songs': 'Song Name', 'Lyrics': 'Song Lyrics', 'Credits': 'Song Credits'}
         sort_asc_dict = {'Ascending': True, 'Descending': False}
@@ -50,6 +65,8 @@ def app():
                 st.write(f'Showing {results_num} Results with "{keyword}" in {field[search_type]}:')
                 st.title('\n\n')
 
+                buttons = list()
+                
                 for i in range(len(results['SongName'])):                
                     songname = results['SongName'][i]
                     artist = results['Artist'][i]
@@ -62,19 +79,52 @@ def app():
                         st.markdown(f'Album: *{album} ({yr})*')
                         
                         
-                    st.button('*Similar Songs*', key=i, help=f'Find Songs with Lyrics Similar to Those of "{songname}" by {artist}')
+                    st.button('*Similar Songs*', key=i, help=f'Find Songs with Lyrics Similar to Those of "{songname}" by {artist}', on_click=on_click())
+                    
+                    
                     st.markdown('\n\n')
                         
             else:
                 st.write(f'No Results Found with "{keyword}" in {field[search_type]}.')
                 
     
-    if service_type == 'Recommendation':
-        recommendation_type = st.radio(
-        'Search for Similar Songs Using:',
-        ('Song', 'Playlist'),
-        horizontal=True
-        )
+    if st.session_state['service_type'] == 'Recommendation':
+        
+        st.write('**Search for Similar Songs**')
+        st.write('\n\n')
+        
+        artists_list = Database.get_all_songs()
+        
+        artists = list()
+        for artist in artists_list:
+            artists.append(re.sub(r"\(|\)|'|,", '', str(artist[0] + ' BY ' + str(artist[1]))))
+            
+        artist_ = st_tags(
+        label='Song(s):',
+        text='Enter A Song Name...',
+        suggestions=artists,
+        maxtags = 10,
+        key='1')
+        
+        #song_list = Database.get_songs(str(artist_))
+        
+        try:
+            song_list = Database.get_songs(str(artist_[0]))
+            
+            
+            songs = list()
+            for song in song_list:
+                songs.append(song)
+
+                st.write(song[1])
+        
+        except:
+            pass
+        
+        
+        #for artist in artists:
+        #    st.write(str(artist))
+
             
 if __name__ == '__main__':
     app()
